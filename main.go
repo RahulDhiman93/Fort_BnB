ca91757c3f49b226c464f44bd57c4ef1529ef276
Rahul Dhiman <rsdm34@gmail.com>
2023-08-10 00:49:54 +0530

    Code segregation

--- main.go
+++ main.go
@@ -1,35 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"text/template"
-)
-
 const portNum = ":8080"
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home_page.tmpl")
-}
-
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about_page.tmpl")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parseTemp, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parseTemp.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template:", err)
-		return
-	}
-}
-
 func main() {
-
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
-
-	fmt.Println("Application listening on ", portNum)
-	http.ListenAndServe(portNum, nil)
+	startWebApp()
 }
