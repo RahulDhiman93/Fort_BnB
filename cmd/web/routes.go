c85c0602345ea6452322a05940fea08aac861ade
Rahul Dhiman <rsdm34@gmail.com>
2023-08-18 18:14:15 +0530

    Middlewares added

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,15 +3,19 @@ package main
 import (
 	"bookingApp/cmd/pkg/config"
 	"bookingApp/cmd/pkg/handlers"
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"net/http"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
