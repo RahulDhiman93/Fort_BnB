bc8813d3f925df4f4e47bc6763ceb3df4cf1267d
Rahul Dhiman <rsdm34@gmail.com>
2023-09-13 18:14:35 -0700

    migration added for users table

---
+++ migrations/20230914011159_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.users (first_name,last_name,email,"password",access_level,created_at,updated_at) VALUES
+    ('Rahul','Dhiman','admin@admin.com','$2a$12$7x4vxzDWamgbBlc0sMLBk.a5FEHpLjREoC06Cx8YHK.eddc2VIarO',3,'2023-09-11 00:00:00.000','2023-09-11 00:00:00.000');
